1a0c0da61bc8cb51e430dfa8698dbc7b94979621
willheeney03 <99232572+willheeney03@users.noreply.github.com>
2024-11-09 20:42:03 -0600

    clean up heuristic code/delete old code and comments

--- bot/evaluate.py
+++ bot/evaluate.py
@@ -6,55 +6,33 @@ def heuristic(board, turn_color):
 
     #initial score
     score = 0
-    chain_score_weight = .8
-    pulse_score_weight = .2
-    # 3 in a row
-    # if(check_win == true):
-    #     score = +infinity
+    chain_score_weight = .6
+    pulse_score_weight = .4
+
     if turn_color in check_winner(board):
-        #print("got here")
-        #print(board)
         return math.inf
-    # two in a row - maybe let chains handle this - maybe check for 2 in a row (blocked/unblocked)
     
-    # check for chains and increase score using quadratic multiplier
-    
+    # Chain Heuristic - (i.e 2 in a row) - maybe add a bonus for 2 in a row being unblocked on both sides? or is pulse handling this?
     chains, chain_sizes = find_chains(board, turn_color)
     chain_score = 0
     for chain_size in chain_sizes:
-        chain_score += chain_size * chain_size
-    chain_score /= 64
-    # apply penalty to having adjacent enemies in your chain
-    # score += penalize_adjacent_enemies_in_chain(board, chains, turn_color)
+        if chain_size > 1:
+            chain_score += chain_size
+    chain_score /= 8
 
-
-    #Find distance between chains:
-    # chain_centers = []
-
-    # for i in range(chains):
-    #     chain_centers[i] = chain_center(i)
-
-    # for i in range(chains-1):
-    #     for j in range(i+1, chains):
-
+    # Gets all of our checkers
     tiles = get_tiles(board)
 
-
+    # Pulse Heuristic
     pulse_weights =[2, 1.5]
-
     pulse_score = 0
+    
     for i in range(len(tiles)):
-        #print("Starting ", i)
-        #print(tiles[i])
         for j in range(len(pulse_weights)):
             pulse_score += get_surrounding_area(board, tiles[i][0], tiles[i][1], pulse_weights[j], pulse_weights[j], j+1)
 
     pulse_score /= 40
-    #find closest peg horizontally, and closest peg vertically
     score += chain_score * chain_score_weight + pulse_score * pulse_score_weight
-    #make into square, other chain peg is corner, horizontally and vertically  are opposite sides
-    
-    #count how many enemy pegs in square, add some 
 
     return score
 
@@ -70,18 +48,7 @@ def get_tiles(board):
     return tiles
 
 def get_surrounding_area(board, row, col, fweight, eweight, radius=1):
-    """
-    Gets the positions surrounding a given cell within a specified radius, applying torus rules (wrapping around edges).
-
-    Parameters:
-    grid (list of list): The 2D grid representing the board.
-    row (int): Row index of the cell to check.
-    col (int): Column index of the cell to check.
-    radius (int): Radius to check around the cell.
-
-    Returns:
-    list of tuples: List of (row, col) positions within the radius.
-    """
+    
     max_row = len(board)
     max_col = len(board[0]) if max_row > 0 else 0
 
@@ -138,34 +105,6 @@ def find_chains(board, turn_color):
     chain_sizes = [len(chain) for chain in chains]
 
     return chains, chain_sizes
-
-def penalize_adjacent_enemies_in_chain(board, chains, turn_color):
-    score = 0
-    board_size = len(board)
-
-    for chain in chains:
-        for row, col in chain:
-            enemy_color = 'B' if turn_color == 'W' else 'W'
-
-            if board[(row - 1) % board_size][col % board_size] == enemy_color:
-                score -= 1
-            if board[(row + 1) % board_size][col % board_size] == enemy_color:
-                score -= 1
-            if board[row % board_size][(col - 1) % board_size] == enemy_color:
-                score -= 1
-            if board[row % board_size][(col + 1) % board_size] == enemy_color:
-                score -= 1
-            if board[(row - 1) % board_size][(col - 1) % board_size] == enemy_color:
-                score -= 1
-            if board[(row + 1) % board_size][(col + 1) % board_size] == enemy_color:
-                score -= 1
-            if board[(row + 1) % board_size][(col - 1) % board_size] == enemy_color:
-                score -= 1
-            if board[(row - 1) % board_size][(col + 1) % board_size] == enemy_color:
-                score -= 1
-
-    return score
-
 
 # Example usage:
 if __name__ == "__main__":
